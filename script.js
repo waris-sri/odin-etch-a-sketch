a0cd391d159a2835edafc6434e89d3d9408f7286
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2025-07-15 01:18:59 +0700

    feat: add drawing when hovered

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.querySelector("#container");
-const containerSize = 500;
+const containerSize = 400;
 container.style.width = container.style.height = `${containerSize}px`;
-const gridSquareNum = 16; // will be replaced with the value from HTML
+const gridSquareNum = 64; // will be replaced with the value from HTML
 const squareSize = containerSize / gridSquareNum;
+const square = document.createElement("div");
 
 // nested loops not needed since flex-wrap already makes it into a square
 for (let i = 0; i < gridSquareNum * gridSquareNum; i++) {
-  const square = document.createElement("div");
-  square.style.width = square.style.height = `${squareSize}px`;
-  container.appendChild(square);
+  const squareGrid = square.cloneNode(true);
+  squareGrid.style.width = squareGrid.style.height = `${squareSize}px`;
+  container.appendChild(squareGrid);
+  squareGrid.addEventListener("mousemove", (e) => {
+    e.target.style.backgroundColor = "red"; // will be replaced with the value from HTML
+  });
 }
