d3e282b4688017197ab51181b426d8ffe084827d
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2025-07-14 23:54:31 +0700

    feat: add the 16x16 grid with JavaScript (flex layout)

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+const container = document.querySelector("#container");
+const containerSize = 500;
+container.style.width = container.style.height = `${containerSize}px`;
+const gridSquareNum = 16; // will be replaced with the value from HTML
+const squareSize = containerSize / gridSquareNum;
+
+// nested loops not needed since flex-wrap already makes it into a square
+for (let i = 0; i < gridSquareNum * gridSquareNum; i++) {
+  const square = document.createElement("div");
+  square.style.width = square.style.height = `${squareSize}px`;
+  container.appendChild(square);
+}
